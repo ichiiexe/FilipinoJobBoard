42cbf741cf7f9bb164f2bdda37553e5bc3428abc
Jefferson <jeff_abuy@hotmail.com>
2026-05-24 01:11:46 +0300

    fixed bug with imports, added register route, and code refactor

--- server/server.js
+++ server/server.js
@@ -2,13 +2,26 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.routes.js";
+
 dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: isDev ? true : process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send(`API running on port ${process.env.PORT}`);
